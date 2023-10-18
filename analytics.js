43b8cd9a0e625f8ca0bf8ebea3291fe39d007006
Parker Morgan <pjm00001@mix.wvu.edu>
2023-10-18 13:31:13 -0400

    possible chart setup for analytics

--- analytics.js
+++ analytics.js
@@ -1,1 +1,17 @@
-document.getElementById('date').innerHTML = new Date().toDateString();
+//code for analytics?
+new Chart("myChart", {
+  type: "doughnut",
+  data: {
+    labels: xValues,
+    datasets: [{
+      backgroundColor: barColors,
+      data: yValues
+    }]
+  },
+  options: {
+    title: {
+      display: true,
+      text: "Genres"
+    }
+  }
+});
