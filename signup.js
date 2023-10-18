7e060990aad206e6ce39bfc5110efba830bd5caf
SoturiVaina <cjp00010@mix.wvu.edu>
2023-10-17 20:13:24 -0400

    Modified signup.js and signup.html

--- signup.js
+++ signup.js
@@ -1,1 +1,29 @@
+window.addEventListener('DOMContentLoaded', domLoaded); // Call domLoaded() when DOM is loaded
 
+function domLoaded() {
+	// Get HTML Elements
+	const button = document.getElementById('button');
+	const username = document.getElementById('username');
+	const password = document.getElementById('password');
+	const confPass = document.getElementById('confirmPassword');
+	
+	// Get database values (with Node.js)
+	
+	
+	// Event on button press; check user input information
+	button.addEventListener('click', function() {
+		const usernameVal = username.value; // Get username value
+		if (usernameVal !== '') {
+			// Check username requirements
+			if ((usernameVal.length < 8) || (usernameVal.length >= 20)) {
+				// CHECK DATABASE
+			}
+			else {
+				// ERROR: USERNAME MUST BE BETWEEN 8 AND 20 CHARACTERS
+			}
+		}
+		else {
+			// ERROR: NO USERNAME PROVIDED
+		}
+	}); 
+}
