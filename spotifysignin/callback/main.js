fda661ffb3744aa294810f9fdc1e7488059624a3
Ethan <143530245+Eb00021@users.noreply.github.com>
2023-10-25 17:26:22 -0400

    Create main.js

---
+++ spotifysignin/callback/main.js
@@ -0,0 +1,16 @@
+populateUI(profile);
+function populateUI(profile) {
+  document.getElementById("displayName").innerText = profile.display_name;
+  if (profile.images[0]) {
+    const profileImage = new Image(200, 200);
+    profileImage.src = profile.images[0].url;
+    document.getElementById("avatar").appendChild(profileImage);
+    document.getElementById("imgUrl").innerText = profile.images[0].url;
+  }
+  document.getElementById("id").innerText = profile.id;
+  document.getElementById("email").innerText = profile.email;
+  document.getElementById("uri").innerText = profile.uri;
+  document.getElementById("uri").setAttribute("href", profile.external_urls.spotify);
+  document.getElementById("url").innerText = profile.href;
+  document.getElementById("url").setAttribute("href", profile.href);
+}
