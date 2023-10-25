673df57d82260977411fc3423573d40f6f74d99f
Ethan <143530245+Eb00021@users.noreply.github.com>
2023-10-25 16:03:11 -0400

    Update spotifysignin.js

--- spotifysignin.js
+++ spotifysignin.js
@@ -26,7 +26,7 @@ export async function redirectToAuthCodeFlow(clientId) {
   const params = new URLSearchParams();
   params.append("client_id", clientId);
   params.append("response_type", "code");
-  params.append("redirect_uri", "https://cs230-signup.eb00021.repl.co/spotifysignin.html/callback");
+  params.append("redirect_uri", "https://wvu-cs230-2023-08-group07.github.io/spotinsights.github.io/spotifysignin.html/callback");
   params.append("scope", "user-read-private user-read-email");
   params.append("code_challenge_method", "S256");
   params.append("code_challenge", challenge);
@@ -59,7 +59,7 @@ export async function getAccessToken(clientId, code) {
   params.append("client_id", clientId);
   params.append("grant_type", "authorization_code");
   params.append("code", code);
-  params.append("redirect_uri", "https://cs230-signup.eb00021.repl.co/spotifysignin.html/callback");
+  params.append("redirect_uri", "https://wvu-cs230-2023-08-group07.github.io/spotinsights.github.io/spotifysignin.html/callback");
   params.append("code_verifier", verifier);
 
   const result = await fetch("https://accounts.spotify.com/api/token", {
