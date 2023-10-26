75fc5be028908fcb25004fd9d5bd84c20142b0f7
Ethan <143530245+Eb00021@users.noreply.github.com>
2023-10-26 13:59:17 -0400

    Update spotifysignin.js

--- spotifysignin/spotifysignin.js
+++ spotifysignin/spotifysignin.js
@@ -14,6 +14,7 @@ document.addEventListener('DOMContentLoaded', function() {
   
 });
 
+
 export async function redirectToAuthCodeFlow(clientId) {
   const verifier = generateCodeVerifier(128);
   const challenge = await generateCodeChallenge(verifier);
