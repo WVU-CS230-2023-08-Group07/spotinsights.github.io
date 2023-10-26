5aaf0f6748e8395a24f193c6e9dd96e545fdbdd5
Ethan <143530245+Eb00021@users.noreply.github.com>
2023-10-26 13:56:30 -0400

    Update main.js

--- spotifysignin/callback/main.js
+++ spotifysignin/callback/main.js
@@ -1,9 +1,12 @@
-populateUI(profile);
+const result = getCookie("result");
+console.log(result);
+populateUI(JSON.parse(result));
+
 function populateUI(profile) {
   document.getElementById("displayName").innerText = profile.display_name;
   if (profile.images[0]) {
     const profileImage = new Image(200, 200);
-    profileImage.src = profile.images[0].url;
+    profileImage.src = profile.images[1].url;
     document.getElementById("avatar").appendChild(profileImage);
     document.getElementById("imgUrl").innerText = profile.images[0].url;
   }
@@ -13,4 +16,18 @@ function populateUI(profile) {
   document.getElementById("uri").setAttribute("href", profile.external_urls.spotify);
   document.getElementById("url").innerText = profile.href;
   document.getElementById("url").setAttribute("href", profile.href);
+}
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var cookies = document.cookie.split(';');
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i];
+        while (cookie.charAt(0) == ' ') {
+            cookie = cookie.substring(1, cookie.length);
+        }
+        if (cookie.indexOf(nameEQ) == 0) {
+            return cookie.substring(nameEQ.length, cookie.length);
+        }
+    }
+    return null;
 }
