e6f195a61fa544fb765703820e1c84aa843d4e9f
Ethan <143530245+Eb00021@users.noreply.github.com>
2023-10-30 16:23:47 -0400

    Update main.js

--- spotifysignin/callback/main.js
+++ spotifysignin/callback/main.js
@@ -1,22 +1,22 @@
 console.log(localStorage.getItem("spotifyInfo"));
-//var urlParts = localStorage.getItem("songURL").split("/");
+var urlParts = localStorage.getItem("songURL").split("/");
 
 
-//var spotifyTrackUrl = "https://open.spotify.com/embed/track/" + urlParts[4];
+var spotifyTrackUrl = "https://open.spotify.com/embed/track/" + urlParts[4];
 
-//const iframe = document.createElement('iframe');
-//iframe.style.borderRadius = '12px';
-//iframe.src = spotifyTrackUrl;
-//iframe.width = '25%';
-//iframe.height = '352';
-//iframe.frameBorder = '0';
-//iframe.allowFullscreen = true;
-//iframe.allow = 'autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture';
-//iframe.loading = 'lazy';
+const iframe = document.createElement('iframe');
+iframe.style.borderRadius = '12px';
+iframe.src = spotifyTrackUrl;
+iframe.width = '25%';
+iframe.height = '352';
+iframe.frameBorder = '0';
+iframe.allowFullscreen = true;
+iframe.allow = 'autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture';
+iframe.loading = 'lazy';
 
-//const spotifyEmbedContainer = document.getElementById('spotifyEmbed');
-//spotifyEmbedContainer.appendChild(iframe);
-//refreshIframe();
+const spotifyEmbedContainer = document.getElementById('spotifyEmbed');
+spotifyEmbedContainer.appendChild(iframe);
+refreshIframe();
 
 populateUI(JSON.parse(localStorage.getItem("spotifyInfo")));
 
@@ -38,20 +38,20 @@ function populateUI(profile) {
   document.getElementById("url").setAttribute("href", profile.href);
 }
 
-//function refreshIframe() {
-//    const iframe = document.getElementById('spotifyEmbed');
-//  fetchCurrentSong(localStorage.getItem("accessToken"))
-//  .then(currentSongData => {
-//    var song = currentSongData;
-//    localStorage.setItem("songURL", song.item.external_urls.spotify);
-// })
-//  .catch(error => {
-//    console.error(error);
-//  });
-//    urlParts = localStorage.getItem("songURL").split("/");
-//    spotifyTrackUrl = "https://open.spotify.com/embed/track/" + urlParts[4];
-//    iframe.src = iframe.src; 
-//}
+function refreshIframe() {
+    const iframe = document.getElementById('spotifyEmbed');
+  fetchCurrentSong(localStorage.getItem("accessToken"))
+  .then(currentSongData => {
+    var song = currentSongData;
+    localStorage.setItem("songURL", song.item.external_urls.spotify);
+ })
+  .catch(error => {
+    console.error(error);
+  });
+    urlParts = localStorage.getItem("songURL").split("/");
+    spotifyTrackUrl = "https://open.spotify.com/embed/track/" + urlParts[4];
+    iframe.src = iframe.src; 
+}
 
 async function fetchCurrentSong(token) {
   const result = await fetch("https://api.spotify.com/v1/me/player/currently-playing", {
