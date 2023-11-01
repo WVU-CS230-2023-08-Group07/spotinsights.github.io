fa3e7157eb304dc8b7879299aaa9391ec08001e2
SoturiVaina <cjp00010@mix.wvu.edu>
2023-10-31 20:11:01 -0400

    Refactored analytics and dashboard pages; added folder for misc JavaScript files

--- dashboard.js
+++ dashboard.js
@@ -1,15 +1,15 @@
 if (localStorage.getItem('spotifyInfo') !== null) {
-  document.getElementById('login-button').style.display = 'none';
-  document.getElementById('signup-button').style.display = 'none';
-  const userProfile = document.getElementById('user-profile');
-  userProfile.style.display = 'block';
-  const spotifyInfo = JSON.parse(localStorage.getItem('spotifyInfo'));
-
-  const profileImageElement = document.getElementById('profile-image');
-  profileImageElement.src = spotifyInfo.images[1].url;
-  profileImageElement.style.display = 'block';
-  
-  const displayNameElement = document.getElementById('display-name');
-  displayNameElement.textContent = spotifyInfo.display_name;
-  displayNameElement.style.display = 'block';
+	document.getElementById('login-button').style.display = 'none';
+	document.getElementById('signup-button').style.display = 'none';
+	const userProfile = document.getElementById('user-profile');
+	userProfile.style.display = 'block';
+	const spotifyInfo = JSON.parse(localStorage.getItem('spotifyInfo'));
+	
+	const profileImageElement = document.getElementById('profile-image');
+	profileImageElement.src = spotifyInfo.images[1].url;
+	profileImageElement.style.display = 'block';
+	
+	const displayNameElement = document.getElementById('display-name');
+	displayNameElement.textContent = spotifyInfo.display_name;
+	displayNameElement.style.display = 'block';
 }
