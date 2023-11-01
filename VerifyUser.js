a909d37be57cd4f9c7920700ade265a1053355a6
CWM00004 <cwm00004@mix.wvu.edu>
2023-11-01 12:45:29 -0400

    commitbaby

--- VerifyUser.js
+++ VerifyUser.js
@@ -1,3 +1,13 @@
+const user = firebase.auth().currentUser;
+
+if (user) {
+  // User is already authenticated
+  // You can perform additional actions if needed
+} else {
+  // User is not authenticated, redirect to login page
+  window.location.href = "login.html";
+}
+
 // VerifyUser.js
 firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
