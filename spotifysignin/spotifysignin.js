4d54e049b988660b5ecbb17f4baaceba67ce1951
CWM00004 <cwm00004@mix.wvu.edu>
2023-11-29 18:19:54 -0500

    Revert "adjustments to firestore"
    This reverts commit 533f9e97b04cb0b146986a702f2f86dc8c1bf679.

--- spotifysignin/spotifysignin.js
+++ spotifysignin/spotifysignin.js
@@ -96,92 +96,90 @@ document.addEventListener('DOMContentLoaded', async function () {
       params.append("code_challenge_method", "S256");
       params.append("code_challenge", challenge);
 
-      localStorage.setItem("authorizationCode", params.toString());
-      await getAccessToken(clientId, code);
-      document.location = `https://accounts.spotify.com/authorize?${params.toString()}`;
+  document.location = `https://accounts.spotify.com/authorize?${params.toString()}`;
+}
+
+function generateCodeVerifier(length) {
+  let text = '';
+  let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+}
+
+async function generateCodeChallenge(codeVerifier) {
+  const data = new TextEncoder().encode(codeVerifier);
+  const digest = await window.crypto.subtle.digest('SHA-256', data);
+  return btoa(String.fromCharCode.apply(null, [...new Uint8Array(digest)]))
+    .replace(/\+/g, '-')
+    .replace(/\//g, '_')
+    .replace(/=+$/, '');
+}
+export async function getAccessToken(clientId, code) {
+  const verifier = localStorage.getItem("verifier");
+
+  const params = new URLSearchParams();
+  params.append("client_id", clientId);
+  params.append("grant_type", "authorization_code");
+  params.append("code", code);
+  params.append("redirect_uri", "https://wvu-cs230-2023-08-group07.github.io/spotinsights.github.io/spotifysignin/");
+  params.append("code_verifier", verifier);
+
+  const result = await fetch("https://accounts.spotify.com/api/token", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: params
+  });
+
+  const { access_token } = await result.json();
+  return access_token;
+}
+
+async function fetchProfile(token) {
+  const result = await fetch("https://api.spotify.com/v1/me", {
+    method: "GET", headers: { Authorization: `Bearer ${token}` }
+  });
+
+  const profile = await result.json();
+
+  // Save Spotify username in Firestore directory based on Firebase UID
+  saveSpotifyInfo(profile.display_name, token);
+
+  return profile;
+}
+
+async function saveSpotifyInfo(spotifyUsername, spotifyToken) {
+  const user = firebase.auth().currentUser;
+
+  if (user) {
+    const userRef = firebase.firestore().collection('private').doc('uids').collection(user.uid);
+
+    try {
+      await userRef.doc('user_data').set({
+        spotifyUsername: spotifyUsername,
+        spotifyToken: spotifyToken,
+        // other user details...
+      });
+
+      console.log('Spotify information stored in Firestore directory');
+    } catch (error) {
+      console.error('Error storing Spotify information in Firestore:', error);
     }
-    
-    function generateCodeVerifier(length) {
-      let text = '';
-      let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    
-      for (let i = 0; i < length; i++) {
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
-      }
-      return text;
-    }
-    // getAccessToken(clientId, code);
-    await redirectToAuthCodeFlow(clientId);
-    
-  // if (!code) {
-  //   redirectToAuthCodeFlow(clientId);
-  // } 
-  //   try {
-  //     const token = await getAccessToken(clientId, code);
-  //     // const profile = await fetchProfile(token);
-  //   } catch (error) {
-  //     console.error('error setting constants or saving data', error);
-  //   }
-});
+  }
+}
 
 
 
-// export async function redirectToAuthCodeFlow(clientId) {
-//   const verifier = generateCodeVerifier(128);
-//   const challenge = await generateCodeChallenge(verifier);
 
-//   localStorage.setItem("verifier", verifier);
-
-//   const params = new URLSearchParams();
-//   params.append("client_id", clientId);
-//   params.append("response_type", "code");
-//   // params.append("redirect_uri", "https://wvu-cs230-2023-08-group07.github.io/spotinsights.github.io/spotifysignin/");
-//   params.append("redirect_uri", "http://localhost:8080/spotifysignin/callback");
-//   params.append("scope", "user-read-private user-read-email user-read-currently-playing user-read-playback-state");
-//   params.append("code_challenge_method", "S256");
-//   params.append("code_challenge", challenge);
-
-//   document.location = `https://accounts.spotify.com/authorize?${params.toString()}`;
-// }
-
-// function generateCodeVerifier(length) {
-//   let text = '';
-//   let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-
-//   for (let i = 0; i < length; i++) {
-//     text += possible.charAt(Math.floor(Math.random() * possible.length));
-//   }
-//   return text;
-// }
-
-//TODO: make this its own function for use in refreshing expired tokens
-// export async function getAccessToken(clientId, code) {
-//   const verifier = localStorage.getItem("verifier");
-
-//   const params = new URLSearchParams();
-//   params.append("client_id", clientId);
-//   params.append("grant_type", "authorization_code");
-//   params.append("code", code);
-//   params.append("redirect_uri", "http://localhost:8080/spotifysignin/callback");
-//   params.append("code_verifier", verifier);
-
-//   const result = await fetch("https://accounts.spotify.com/api/token", {
-//     method: "POST",
-//     headers: { "Content-Type": "application/x-www-form-urlencoded" },
-//     body: params
-//   });
-
-//   const { access_token } = await result.json();
-//   return access_token;
-// }
-
-// async function fetchCurrentSong(token) {
-//   const result = await fetch("https://api.spotify.com/v1/me/player/currently-playing", {
-//     method: "GET", headers: { Authorization: `Bearer ${token}` }
-//   });
-
-//   return result.json();
-// }
+async function fetchCurrentSong(token) {
+  const result = await fetch("https://api.spotify.com/v1/me/player/currently-playing", {
+    method: "GET", headers: { Authorization: `Bearer ${token}` }
+  });
+  
+  return result.json();
+}
 
 // async function populateUI(profile) {
 //   localStorage.setItem("spotifyInfo", JSON.stringify(profile));
