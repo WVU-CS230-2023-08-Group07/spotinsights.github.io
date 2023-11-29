7feb2db14d8c0b3ea71c5c0c6af016ae09ee9487
CWM00004 <cwm00004@mix.wvu.edu>
2023-11-29 12:26:26 -0500

    stuck

--- js/login.js
+++ js/login.js
@@ -1,12 +1,13 @@
-	/*const firebaseConfig = {
-		apiKey: "AIzaSyCiaoHpxeFZWA4gb6NJzMGunj7ytNSChKY",
-		authDomain: "spotinsights.firebaseapp.com",
-		projectId: "spotinsights",
-		storageBucket: "spotinsights.appspot.com",
-		messagingSenderId: "293805645735",
-		appId: "1:293805645735:web:490f3ccf4444b613f16082"
-	};
-	firebase.initializeApp(firebaseConfig); */
+	// const firebaseConfig = {
+	// 	apiKey: "AIzaSyCiaoHpxeFZWA4gb6NJzMGunj7ytNSChKY",
+	// 	authDomain: "spotinsights.firebaseapp.com",
+	// 	projectId: "spotinsights",
+	// 	storageBucket: "spotinsights.appspot.com",
+	// 	messagingSenderId: "293805645735",
+	// 	appId: "1:293805645735:web:490f3ccf4444b613f16082"
+	// };
+	// firebase.initializeApp(firebaseConfig);
+
   function signIn() {
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
