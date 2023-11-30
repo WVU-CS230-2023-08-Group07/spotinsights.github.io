7215efeb41b4fb090d2fe28f8e0c73056306d167
SoturiVaina <cjp00010@mix.wvu.edu>
2023-11-30 07:54:28 -0500

    Changed login page CSS and refactored some code

--- js/login.js
+++ js/login.js
@@ -1,65 +1,28 @@
-	/*const firebaseConfig = {
-		apiKey: "AIzaSyCiaoHpxeFZWA4gb6NJzMGunj7ytNSChKY",
-		authDomain: "spotinsights.firebaseapp.com",
-		projectId: "spotinsights",
-		storageBucket: "spotinsights.appspot.com",
-		messagingSenderId: "293805645735",
-		appId: "1:293805645735:web:490f3ccf4444b613f16082"
-	};
-	firebase.initializeApp(firebaseConfig); */
-  function signIn() {
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-
-    firebase.auth().signInWithEmailAndPassword(email, password)
-      .then((userCredential) => {
-        // User signed in successfully
-        const user = userCredential.user;
-        console.log(user);
-        window.location.href = 'dashboard.html';
-      })
-      .catch((error) => {
-        console.error(error.message);
-        window.alert("Invalid user credentials");
-      });
-  }
-
-	function signOut() {
-	firebase.auth().signOut()
-      .then(() => {
-        // User signed out successfully
-        console.log('User signed out');
-        window.location.href = 'index.html';
-      })
-      .catch((error) => {
-        console.error(error.message);
-      });
-    }
-/* function domLoaded() {
-	// Get HTML Elements
-	const button = document.getElementById('login-button');
-	const email = document.getElementById('email');
-	const password = document.getElementById('password');
-	const loginError = document.getElementById('login-error');
+function signIn() {
+	const email = document.getElementById('email').value;
+	const password = document.getElementById('password').value;
 	
-	// Event on button press; check user input information
-	button.addEventListener('click', (e) => {
-		e.preventDefault(); // Prevent the form from submitting
-		
-		const emailVal = email.value;
-		const passwordVal = password.value;
-		
-		// Sign in the user
-		signIn(emailVal, passwordVal)
-			.then((user) => {
-				console.log("Sign-in successful:", user);
-				window.location.href = 'dashboard.html'; // Redirect to dashboard.html
-			})
-			.catch((error) => {
-				console.error("Sign-in failed:", error);
-				// Handle login errors to the user
-				loginError.innerText = 'Invalid username or password. Please try again.';
-			});
-	});
+	firebase.auth().signInWithEmailAndPassword(email, password)
+		.then((userCredential) => {
+			// User signed in successfully
+			const user = userCredential.user;
+			console.log(user);
+			window.location.href = 'dashboard.html';
+		})
+		.catch((error) => {
+			console.error(error.message);
+			window.alert("Invalid user credentials");
+		});
 }
-*/
+
+function signOut() {
+	firebase.auth().signOut()
+		.then(() => {
+			// User signed out successfully
+			console.log('User signed out');
+			window.location.href = 'index.html';
+		})
+		.catch((error) => {
+			console.error(error.message);
+		});
+}
--- js/signup.js
+++ js/signup.js
@@ -1,64 +1,16 @@
-	function signUp() {
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-
-    firebase.auth().createUserWithEmailAndPassword(email, password)
-      .then((userCredential) => {
-        // User signed up successfully
-        const user = userCredential.user;
-        console.log(user);
-        window.location.href = 'login.html';
-      })
-      .catch((error) => {
-        console.error(error.message);
-        window.alert(error.message);
-      });
-  }
-
-
-
-/* window.addEventListener('DOMContentLoaded', domLoaded); // Call domLoaded() when DOM is loaded
-
-function domLoaded() {
-    // Get HTML Elements
-    const button = document.getElementById('signup-button'); // Update the ID to match your HTML
-    const email = document.getElementById('email'); // Update the ID to match your HTML
-    const password = document.getElementById('password');
-    const confirmPassword = document.getElementById('confirm-password'); // Update the ID to match your HTML
-    const signupError = document.getElementById('signup-error');
-
-    // Event on button press; check user input information
-    button.addEventListener('click', function (e) {
-        e.preventDefault(); // Prevent the form from submitting
-
-        const emailVal = email.value;
-        const passwordVal = password.value;
-        const confirmPasswordVal = confirmPassword.value;
-
-        if (emailVal !== '' && passwordVal !== '' && confirmPasswordVal !== '') { // Check fields are not empty
-            if (passwordVal.length >= 8) { // Check password is >= 8 characters
-                if (passwordVal === confirmPasswordVal) { // Check password and confirmPassword are the same
-					
-					// Sign up the user
-                    signUp(emailVal, passwordVal)
-						.then((user) => {
-							console.log("Sign-up successful:", user);
-							window.location.href = 'dashboard.html'; // Redirect to dashboard.html
-						})
-						.catch((error) => {
-							console.error("Sign-up failed:", error);
-							// Handle signup errors to the user
-							signupError.innerText = error;
-						});
-				
-                } else {
-                    signupError.innerText = 'Passwords do not match.';
-                }
-            } else {
-                signupError.innerText = 'Password must be at least 8 characters long.';
-            }
-        } else {
-            signupError.innerText = 'All fields are required.';
-        }
-    });
-} */
+function signUp() {
+	const email = document.getElementById('email').value;
+	const password = document.getElementById('password').value;
+	
+	firebase.auth().createUserWithEmailAndPassword(email, password)
+		.then((userCredential) => {
+			// User signed up successfully
+			const user = userCredential.user;
+			console.log(user);
+			window.location.href = 'login.html';
+		})
+		.catch((error) => {
+			console.error(error.message);
+			window.alert(error.message);
+		});
+}
