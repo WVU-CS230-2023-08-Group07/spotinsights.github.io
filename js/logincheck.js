83907fc6cbac6cba18dfdc210810e7234c989835
tsuchinokoReal <parkermorgan@rocketmail.com>
2023-12-07 17:10:44 -0500

    testing fix

--- js/logincheck.js
+++ js/logincheck.js
@@ -1,7 +1,6 @@
 const user = firebase.auth().currentUser;
 
 firebase.auth().onAuthStateChanged(function(user){
-	firebase.auth().signOut();
 	if(!user){
 		window.location.href = 'login.html'; 
 	}
