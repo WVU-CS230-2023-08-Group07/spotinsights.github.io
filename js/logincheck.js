e647d4e78adcf17b4bc426e12e713b8004f8a584
tsuchinokoReal <parkermorgan@rocketmail.com>
2023-12-07 17:00:08 -0500

    restoring logout functionality

--- js/logincheck.js
+++ js/logincheck.js
@@ -2,6 +2,7 @@ const user = firebase.auth().currentUser;
 
 firebase.auth().onAuthStateChanged(function(user){
 	if(!user){
+		firebase.auth().signOut();
 		window.location.href = 'login.html'; 
 	}
 });
