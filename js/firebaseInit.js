35dc3a6e788eeca0cc6ad6f94e68f4b3aa0c09df
tsuchinokoReal <parkermorgan@rocketmail.com>
2023-12-07 17:28:05 -0500

    undoing edits

--- js/firebaseInit.js
+++ js/firebaseInit.js
@@ -1,4 +1,3 @@
-import { initializeApp } from "firebase/app";
 const firebaseConfig = {
 	apiKey: "AIzaSyCiaoHpxeFZWA4gb6NJzMGunj7ytNSChKY",
 	authDomain: "spotinsights.firebaseapp.com",
@@ -7,4 +6,4 @@ const firebaseConfig = {
 	messagingSenderId: "293805645735",
 	appId: "1:293805645735:web:490f3ccf4444b613f16082"
 };
-const app = initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
